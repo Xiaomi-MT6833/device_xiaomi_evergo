58a99f8f17dfcd843929dff5fed5f600e678981d
Nolen Johnson <johnsonnolen@gmail.com>
2022-09-20 20:43:05 +0530

    evergo: Simplify AVB flag logic
    * AVB's `--flags 3` arguement builds a disabled VBMeta with disabled hashtree, so no need to set both flags individually.
    
    Change-Id: Ibe9ee322a5af58f13e76d53e18dee49451fe8838
    Signed-off-by: Sarthak <sarthakroy2002@gmail.com>
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,8 +32,7 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
 # Android Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := evergo
