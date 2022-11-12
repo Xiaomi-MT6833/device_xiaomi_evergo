28d93e57d939e0d38c84cbf192b93ba49fbeddd8
Sushrut1101 <guptasushrut@gmail.com>
2022-11-12 18:43:37 +0530

    evergo: Set the Build Fingerprint and info
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- arrow_evergo.mk
+++ arrow_evergo.mk
@@ -4,18 +4,30 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific fist.
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from device makefile
-$(call inherit-product, $(LOCAL_PATH)/device.mk)
-
-# Inherit some common ArrowOS Stuff
+# Inherit some common ArrowOS stuff.
 $(call inherit-product, vendor/arrow/config/common.mk)
 
-PRODUCT_NAME := arrow_evergo
+# Inherit from evergo device
+$(call inherit-product, device/xiaomi/evergo/device.mk)
+
+# ArrowOS Stuff
+TARGET_BOOT_ANIMATION_RES := 720
+
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := evergo
-PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_NAME := arrow_evergo
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11T 5G
+PRODUCT_MANUFACTURER := xiaomi
+
+# Build info
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="evergo-user 12 SP1A.210812.016 V13.0.5.0.SGBINXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/evergo_in/evergo:12/SP1A.210812.016/V13.0.5.0.SGBINXM:user/release-keys
