96b1e64659d33e44ab32c6db77db9605200bbfe4
Sushrut1101 <guptasushrut@gmail.com>
2022-11-16 17:33:40 +0530

    evergo: Import `vendor.prop`
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -104,6 +104,7 @@ TARGET_BOARD_PLATFORM := mt6833
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
 BOARD_USES_RECOVERY_AS_BOOT := true
