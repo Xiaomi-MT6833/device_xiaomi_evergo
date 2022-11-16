0b6292fe02398b820f26de9cc1b0757b12f8cbe6
Sushrut1101 <guptasushrut@gmail.com>
2022-11-16 18:05:17 +0530

    evergo: Build IR HAL
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -81,6 +81,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-impl.recovery
 
+# IR
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-service \
+    android.hardware.ir@1.0-impl
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
